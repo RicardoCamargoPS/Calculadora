abdddc08a6515b7d5c2d6f8851116734858f1145
Ricardo de Camargo <ricardocamargoprado@hotmail.com>
2023-10-30 20:27:36 -0300

    inicio da refatoração de todo o codigo

--- Calculadora/src/com/calculadora/calculadora/calculadoraMain.java
+++ Calculadora/src/com/calculadora/calculadora/calculadoraMain.java
@@ -1,8 +1,30 @@
 package com.calculadora.calculadora;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 
 public class calculadoraMain extends JFrame {
+
+	private final int WIDTH = 460;
+	private final int HEIGHT = 300;
+
+	private display visor;
+
+	public calculadoraMain(){
+
+		setSize(WIDTH, HEIGHT);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setResizable(false);
+
+		BoxLayout layout = new BoxLayout(Y_AXIS, 2);
+		setLayout(layout);
+
+		visor = new display(WIDTH, HEIGHT);
+		
+
+	}
 
 	public static void main(String[] args) {
 		
--- Calculadora/src/com/calculadora/calculadora/display.java
+++ Calculadora/src/com/calculadora/calculadora/display.java
@@ -2,32 +2,48 @@ package com.calculadora.calculadora;
 
 import javax.swing.JPanel;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 public class display {
 
     private JPanel displayPanel;
     private Canvas canvas;
+    private BufferedImage layer;
 
-    private int whidth, height;
+    private int width, height;
 
     public display(int whidth, int height){
 
-        this.whidth = whidth - 10;
+        this.width = whidth - 10;
         this.height = height / 4;
 
         canvas = new Canvas();
-        canvas.setSize(new Dimension(this.whidth, this.height));
+        canvas.setSize(new Dimension(this.width, this.height));
 
         displayPanel = new JPanel();
         displayPanel.add(canvas);
     }
 
-    public BufferStrategy getBufferStrategy(){  
-        return canvas.getBufferStrategy();
+
+    public void render(){
+        BufferStrategy bs = canvas.getBufferStrategy();
+        if(bs == null){
+           canvas.createBufferStrategy(3);
+            bs = canvas.getBufferStrategy();
+        }
+        Graphics g = layer.getGraphics();
+        g.setColor(Color.white);
+        g.fillRect(0, 0, width, height);        
+                                                              
+                                
+        g = bs.getDrawGraphics();
+        g.drawImage(layer, 0, 0, width, height, null);
+        g.dispose();
+        bs.show();
     }
-    public void createBufferStrategy(){
-        canvas.createBufferStrategy(3);
-    }    
+
 }
